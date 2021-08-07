7e615c9d3c18ab8cb82e7cc565056e5285eec6fe
k0ndrat <88559542+k0ndrat@users.noreply.github.com>
2021-08-07 21:08:14 +0300

    Initial Commit

---
+++ deginskuhnya/deginskuhnyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  deginskuhnyaApp.swift
+//  deginskuhnya
+//
+//  Created by Dima Kondratiev on 07.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct deginskuhnyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
